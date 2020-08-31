fd5110ad0dd2f8e2796c4b0547e0429615379e85
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-31 15:54:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,23 @@
-# require modules here
+require 'pry'
+require "yaml"
+# emoticon_library = YAML.load_file('./lib/emoticons.yml')
 
-def load_library
-  # code goes here
+def load_library(file)
+  emoticon_library = YAML.load_file(file)
+  translator_hash = Hash.new
+  emoticon_library.each do |key, emoticons|
+    translator_hash << [:emotion] = (key)
+    emoticons.each do |eng_emotion|
+      translator_hash[emotion:] = [key]
+    binding.pry
+    end
+  end
 end
-
-def get_japanese_emoticon
-  # code goes here
-end
-
-def get_english_meaning
-  # code goes here
-end
+#
+# def get_japanese_emoticon
+#   # code goes here
+# end
+#
+# def get_english_meaning
+#   # code goes here
+# end
