f13e6a3f30f15352a2d0b1a2787a71568d132da0
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-31 16:34:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,8 +5,9 @@ require "yaml"
 def load_library(file)
   file = YAML.load_file("./lib/emoticons.yml")
     file.each_with_object({}) do |name, emoticons, translator_hash|
+  binding.pry
      emoticons.each do |val_1, val_2|
-       binding.pry
+
        emoticons[0] = val_1
        emoticons[1] = val_2
        new_hash = {name: {english: emoticons[0], japanese: emoticons[1]}}
